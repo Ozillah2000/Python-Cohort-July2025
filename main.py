7f312f9add6a9bc000a984a2d1f1f0b09be59a8c
Ozillah2000 <daveochieng2@gmail.com>
2025-07-30 14:40:56 +0300

    added

---
+++ main.py
@@ -0,0 +1,15 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+sum = num1 + num2
+print("The sum of", num1, "and", num2, "is:", sum)
+product = num1 * num2
+print("The product of", num1, "and", num2, "is:", product)  
+difference = num1 - num2
+print("The difference of", num1, "and", num2, "is:", difference)
+quotient = num1 / num2
+print("The quotient of", num1, "and", num2, "is:", quotient)
+remainder = num1 % num2
+print("The remainder of", num1, "and", num2, "is:", remainder)
+exponentiation = num1 ** num2
+print("The exponentiation of", num1, "and", num2, "is:", exponentiation)
